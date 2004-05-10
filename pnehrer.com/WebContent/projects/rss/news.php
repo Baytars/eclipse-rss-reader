e1fe78be16faf1b21764ea5d90409dcef57bca54
pnehrer <>
2004-05-10 04:55:03 +0000

    Fixed file encoding.

--- pnehrer.com/WebContent/projects/rss/news.php
+++ pnehrer.com/WebContent/projects/rss/news.php
@@ -12,7 +12,11 @@
 		<TITLE>RSS Reader News</TITLE><!-- /tpl:put -->
 </head>
 <body onload="javascript:return body_onload();"><div
-	class="searchForm"><!-- siteedit:navbar target="topchildren" spec="/presence/theme/topnav.html" --><!-- /siteedit:navbar -->Search: <input
+	class="searchForm"><!-- siteedit:navbar target="topchildren" spec="/presence/theme/topnav.html" -->
+<A class="extLink" href="/presence/home/index.html">Home</A> |
+<A class="extLink" href="/presence/home/resume.html">Resume</A> |
+<A class="extLink" href="/presence/projects/index.html">Projects</A> |
+<!-- /siteedit:navbar -->Search: <input
 	class="searchText" 
 	type="text" 
 	name="q"
@@ -30,7 +34,42 @@
 	cellpadding="0" 
 	cellspacing="0">
 	<tr>
-		<td valign="top" align="center" width="150" rowspan="2"><!-- siteedit:sitemap spec="/presence/theme/leftnav.html" onlychilden="true" targetlevel="1-4" --><!-- /siteedit:sitemap --></td>
+		<td valign="top" align="center" width="150" rowspan="2"><!-- siteedit:sitemap spec="/presence/theme/leftnav.html" onlychilden="true" targetlevel="1-4" -->
+<IMG src="/presence/theme/blackdot.gif" width="2" height="16" alt="extends"><TABLE width="100%" cellpadding="0" cellspacing="0">
+<TBODY><TR><TD>
+<DIV class="menuBox">
+<DIV class="menu"><A href="/presence/home/index.html"><IMG src="/presence/theme/icons/home.png" alt="Home" class="icon"> Home</A></DIV>
+<DIV class="menu"><A href="/presence/home/resume.html"><IMG src="/presence/theme/icons/resume.png" alt="Resume" class="icon"> Resume</A></DIV>
+<DIV class="menuAncestor"><A href="/presence/projects/index.html"><IMG src="/presence/theme/icons/projects.png" alt="Projects" class="icon"> Projects</A></DIV>
+</DIV>
+</TD><TD width="8">
+</TD></TR>
+</TBODY></TABLE>
+<IMG src="/presence/theme/blackdot.gif" width="2" height="16" alt="extends"><TABLE width="100%" cellpadding="0" cellspacing="0">
+<TBODY><TR><TD>
+<DIV class="menuBox">
+<DIV class="menu"><A href="/presence/home/about.html"><IMG src="/presence/theme/icons/about.png" alt="Presence" class="icon"> Presence</A></DIV>
+<DIV class="menu"><A href="/presence/projects/cep/index.html"><IMG src="/presence/theme/icons/folder_blue_open.png" alt="Castor Plug-in" class="icon"> Castor Plug-in</A></DIV>
+<DIV class="menuAncestor"><A href="/presence/projects/rss/index.html"><IMG src="/presence/theme/icons/xml_16.gif" alt="RSS Reader" class="icon"> RSS Reader</A></DIV>
+<DIV class="menu"><A href="/presence/projects/jelly4eclipse/index.html"><IMG src="/presence/theme/icons/folder_blue_open.png" alt="Eclipse Scripting" class="icon"> Eclipse Scripting</A></DIV>
+</DIV>
+</TD><TD width="8">
+</TD></TR>
+</TBODY></TABLE>
+<IMG src="/presence/theme/blackdot.gif" width="2" height="16" alt="extends"><TABLE width="100%" cellpadding="0" cellspacing="0">
+<TBODY><TR><TD>
+<DIV class="menuBox">
+<DIV class="menuSelf"><IMG src="/presence/theme/icons/item_new_16.gif" alt="News" class="icon"> News</DIV>
+<DIV class="menu"><A href="/presence/projects/rss/download.html"><IMG src="/presence/theme/icons/update_16.png" alt="Download" class="icon"> Download</A></DIV>
+<DIV class="menu"><A href="/presence/projects/rss/features.html"><IMG src="/presence/theme/icons/navigator_16.png" alt="Features" class="icon"> Features</A></DIV>
+<DIV class="menu"><A href="/presence/projects/rss/usage.html"><IMG src="/presence/theme/icons/browse_16.png" alt="Usage" class="icon"> Usage</A></DIV>
+<DIV class="menu"><A href="/presence/projects/rss/screenshots.html"><IMG src="/presence/theme/icons/view_16.png" alt="Screenshots" class="icon"> Screenshots</A></DIV>
+</DIV>
+</TD><TD width="8">
+<IMG src="/presence/theme/blackdot.gif" width="8" height="2" alt="aggregates">
+</TD></TR>
+</TBODY></TABLE>
+<!-- /siteedit:sitemap --></td>
 		<td height="8"></td>
 	</tr>
 	<tr>
