3bfe6173dcb96c35b1ad493305657813f01c98af
pnehrer <>
2004-05-10 04:35:06 +0000

    Fixed editor association.

--- pnehrer.com/WebContent/projects/rss/news.php
+++ pnehrer.com/WebContent/projects/rss/news.php
@@ -1,46 +1,40 @@
-<!-- tpl:insert page="/theme/home.htpl" --><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<HTML>
-<HEAD>
-<META http-equiv="Content-Type"
+<!-- tpl:insert page="/theme/home.htpl" --><!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type"
 	content="text/html; charset=ANSI_X3.4-1968">
-<META name="GENERATOR" content="IBM WebSphere Studio">
-<META http-equiv="Content-Style-Type" content="text/css">
+<meta name="GENERATOR" content="IBM WebSphere Studio">
 <SCRIPT language="JavaScript" src="/presence/theme/updates.js"></SCRIPT>
-<LINK href="/presence/theme/blue.css" rel="stylesheet" type="text/css">
-<LINK rel="shortcut icon" href="/presence/theme/icons/favicon.ico" type="image/x-icon">
+<link rel="stylesheet" href="/presence/theme/blue.css" type="text/css">
+<SCRIPT language="JavaScript" src="/presence/theme/search.js"></SCRIPT>
+<link rel="shortcut icon" href="/presence/theme/icons/favicon.ico" type="image/x-icon">
 <!-- tpl:put name="head" -->
 		<TITLE>RSS Reader News</TITLE><!-- /tpl:put -->
-</HEAD>
-<BODY onload="javascript:return body_onload();">
-<DIV class="extLinkDiv"><SPAN style="vertical-align: bottom"><A class="extLink" href="http://pnehrer.freeshell.org/presence/projects/rss/index.html">pnehrer.com</A> | <A class="extLink" href="http://sourceforge.net/projects/morphine">SourceForge.net</A></SPAN></DIV>
-<DIV class="header"><A href="/presence/projects/rss/index.html"><IMG border="0"
-	src="/presence/theme/icons/rss-banner.png" width="500" height="64" alt="Eclipse RSS Reader"></A></DIV>
-<!-- siteedit:navbar target="topchildren" spec="/presence/theme/leftnav.html" -->
-<UL class="menuBox">
-<LI class="menu" style='list-style-image: url("/presence/theme/icons/detail_16.png")'><A class="leftNavLink" href="/presence/projects/rss/index.html">About</A></LI>
-<LI class="menuSelf" style='list-style-image: url("/presence/theme/icons/item_new_16.gif")'>News</LI>
-<LI class="menu" style='list-style-image: url("/presence/theme/icons/update_16.png")'><A class="leftNavLink" href="/presence/projects/rss/download.html">Download</A></LI>
-<LI class="menu" style='list-style-image: url("/presence/theme/icons/navigator_16.png")'><A class="leftNavLink" href="/presence/projects/rss/features.html">Features</A></LI>
-<LI class="menu" style='list-style-image: url("/presence/theme/icons/browse_16.png")'><A class="leftNavLink" href="/presence/projects/rss/usage.html">Usage</A></LI>
-<LI class="menu" style='list-style-image: url("/presence/theme/icons/view_16.png")'><A class="leftNavLink" href="/presence/projects/rss/screenshots.html">Screenshots</A></LI>
-</UL>
-<!-- /siteedit:navbar -->
-<DIV style="float: right; clear: right; margin: 4px"><script type="text/javascript"><!--
-google_ad_client = "pub-5464643717210088";
-google_ad_width = 160;
-google_ad_height = 600;
-google_ad_format = "160x600_as";
-google_ad_channel ="";
-google_color_border = "FF6600";
-google_color_bg = "FFFFFF";
-google_color_link = "FF6600";
-google_color_url = "FF6600";
-google_color_text = "000000";
-//--></script>
-<script type="text/javascript"
-  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-</script></DIV>
-<DIV class="content"><!-- tpl:put name="body" -->
+</head>
+<body onload="javascript:return body_onload();"><div
+	class="searchForm"><!-- siteedit:navbar target="topchildren" spec="/presence/theme/topnav.html" --><!-- /siteedit:navbar -->Search: <input
+	class="searchText" 
+	type="text" 
+	name="q"
+	size="12"
+	onkeypress="search(this, event)"></div><div 
+	class="header"><a 
+	href="/presence/home/index.html"><img 
+	border="0"
+	src="/presence/theme/banner.png" 
+	width="320" 
+	height="32"
+	alt="pnehrer.com"></a></div><table
+	width="100%" 
+	border="0" 
+	cellpadding="0" 
+	cellspacing="0">
+	<tr>
+		<td valign="top" align="center" width="150" rowspan="2"><!-- siteedit:sitemap spec="/presence/theme/leftnav.html" onlychilden="true" targetlevel="1-4" --><!-- /siteedit:sitemap --></td>
+		<td height="8"></td>
+	</tr>
+	<tr>
+		<td valign="top" class="content"><!-- tpl:put name="body" -->
 <?php
 	$fragment = "news.html";
 	if($_SERVER["HTTP_HOST"] != "morphine.sourceforge.net")
@@ -48,10 +42,8 @@ google_color_text = "000000";
 
 	include($fragment);
 ?>
-<!-- /tpl:put --></DIV>
-<DIV class="footer">Copyright &copy; 2003, 2004 <a
-	href="mailto:pnehrer@users.sourceforge.net">Peter Nehrer</a>.<br>
-Version $Id$.</DIV>
-</BODY>
-</HTML>
+<!-- /tpl:put --></td>
+	</tr>
+</table><div class="footer">Copyright &copy; 2003, 2004 <a href="mailto:pnehrer@freeshell.org">Peter Nehrer</a>.<br>Version $Id$.</div></body>
+</html>
 <!-- /tpl:insert -->
